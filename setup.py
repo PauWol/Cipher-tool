50b46eb81760938eda7b3a0f88a070089942d9c9
PauWol <145593735+PauWol@users.noreply.github.com>
2024-04-26 07:48:37 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,22 +5,28 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
+        'collections',
         'colorama',
-        # Add any other dependencies your project needs here
+        'tqdm'
     ],
     entry_points={
         'console_scripts': [
             'cipher-tool = cipher_tool.main:main',
         ],
     },
-    author='Your Name',
+    author='Paul Wolf',
     author_email='your.email@example.com',
     description='A tool for encryption and decryption using Caesar and Substitution ciphers',
     keywords='encryption decryption caesar substitution cipher',
-    url='https://github.com/yourusername/cipher_tool',
-    classifiers=[
+    url='https://github.com/PauWol/Cipher-tool',
+       classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3',
 )
